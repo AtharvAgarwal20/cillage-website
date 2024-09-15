08c3793c442fb26563f187cdb5dc07639d206f91
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-09-15 12:28:30 +0530

    feat: added provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { GlobalProvider } from "./store/global-context.tsx";
 
 import Landing from "./routes/Landing.tsx";
 
@@ -15,6 +16,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <GlobalProvider>
+      <RouterProvider router={router} />
+    </GlobalProvider>
   </StrictMode>
 );
